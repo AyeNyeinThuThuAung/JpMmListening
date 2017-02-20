3b5127dbd9ca7249d94bb36454fa11010c0d0ef3
Aye Nyein <ayenyeinthuthuaung@op-g.co.jp>
2017-02-20 13:08:46 +0900

    1:08 JpMm

--- app/src/main/java/com/example/aye/jpmmlistening/DownloadActivity.java
+++ app/src/main/java/com/example/aye/jpmmlistening/DownloadActivity.java
@@ -4,19 +4,25 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Rating;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.List;
 
 
 public class DownloadActivity extends Activity {
-    Context context = this;
 
+    Context context = this;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //show download page
         setContentView(R.layout.download);
+        //AsynTask execute
         new ProgressActivity().execute(0, 0, 0);
     }
 
--- app/src/main/java/com/example/aye/jpmmlistening/PlayQuizActivity.java
+++ app/src/main/java/com/example/aye/jpmmlistening/PlayQuizActivity.java
@@ -3,13 +3,12 @@ package com.example.aye.jpmmlistening;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-/**
- * Created by Aye on 2/17/2017.
- */
 
+//プレーズ3択クイズ
 public class PlayQuizActivity extends Activity {
     Context context = this;
     @Override
@@ -18,9 +17,22 @@ public class PlayQuizActivity extends Activity {
         setContentView(R.layout.playquiz);
     }
 
+    //show dialog 正解数
     public void mark(View view){
         Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.checkmark);
         dialog.show();
+    }
+
+    //click メニューへ戻る
+    public void returnEpisode(View view){
+        Intent intent = new Intent(PlayQuizActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    //click もう一度挑戦
+    public void tryAgain(View view){
+        Intent intent = new Intent(PlayQuizActivity.this,QuizzModeActivity.class);
+        startActivity(intent);
     }
 }
