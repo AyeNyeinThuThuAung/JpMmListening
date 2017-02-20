a003018673d34f0202032696a3b588c4821ebfc2
Aye Nyein <ayenyeinthuthuaung@op-g.co.jp>
2017-02-20 16:00:50 +0900

    4hr JpMm

--- app/src/main/java/com/example/aye/jpmmlistening/DownloadActivity.java
+++ app/src/main/java/com/example/aye/jpmmlistening/DownloadActivity.java
@@ -9,6 +9,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.List;
 public class DownloadActivity extends Activity {
 
     Context context = this;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,7 @@ public class DownloadActivity extends Activity {
         setContentView(R.layout.download);
         //AsynTask execute
         new ProgressActivity().execute(0, 0, 0);
+
     }
 
     class ProgressActivity extends AsyncTask<Integer, Integer, Integer> {
@@ -41,26 +44,28 @@ public class DownloadActivity extends Activity {
         @Override
         protected void onPostExecute(Integer integer) {
             super.onPostExecute(integer);
-            Dialog dialog = new Dialog(context);
-            dialog.setContentView(R.layout.modechoice);
-            dialog.show();
+            Intent intent = new Intent(DownloadActivity.this,ModeChoiceActivity.class);
+            startActivity(intent);
+//            Dialog dialog = new Dialog(context);
+//            dialog.setContentView(R.layout.modechoice);
+//            dialog.show();
         }
     }
 
-    public void selectmode(View view) {
-        switch (view.getId()) {
-            case R.id.listening:
-                Intent intent = new Intent(DownloadActivity.this, ListeningModeActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.quezz:
-                Intent intent1 = new Intent(DownloadActivity.this, QuizzModeActivity.class);
-                startActivity(intent1);
-                break;
-            case R.id.repating:
-                Intent intent2 = new Intent(DownloadActivity.this, JapanActivity.class);
-                startActivity(intent2);
-        }
-    }
+//    public void selectmode(View view) {
+//        switch (view.getId()) {
+//            case R.id.listening:
+//                Intent intent = new Intent(DownloadActivity.this, ListeningModeActivity.class);
+//                startActivity(intent);break;
+//
+//            case R.id.repating:
+//                Intent intent1 = new Intent(DownloadActivity.this, RepatingActivity.class);
+//                startActivity(intent1);break;
+//
+//            case R.id.quezz:
+//                Intent intent2 = new Intent(DownloadActivity.this,QuizzModeActivity.class);
+//                startActivity(intent2);
+//        }
+//    }
 
 }
